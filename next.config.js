08eb55a82937d4de4915070955f475fd56d949ca
Brian Koehler <briandkoehler@gmail.com>
2022-02-07 15:13:47 -0500

    Whitelist media.giphy.com for image domains

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     reactStrictMode: true,
     images: {
-        domains: ['localhost', process.env.CMS_URL]
+        domains: ['localhost', 'media.giphy.com', process.env.CMS_URL]
     },
     webpack: (config, { webpack }) => {
         /* Hide error "Critical dependency: the request of a dependency is an expression" from remark-textr */
